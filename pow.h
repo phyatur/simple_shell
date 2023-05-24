8b24d892ac0543ae9ce8dec7eeed594288a272ed
phyatur <phyatur.official@gmail.com>
2023-05-24 09:55:53 +0000

    New repo to check with checker :/

---
+++ pow.h
@@ -0,0 +1,16 @@
+#ifndef POW_H
+#define POW_H
+
+void print_var(char *var_name);
+
+void print_exit_status(char exit_status_str[],
+
+int *exit_status_len,
+
+int exit_status, int stdout_fd);
+
+void print_pid(char pid_str[], int *pid_len, int stdout_fd);
+
+void print_variable(char arg[]);
+
+#endif
